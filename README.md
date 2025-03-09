103275cc9dbf1b5673ee4d96fdb0ae0cee7d8dd8
Hippolyte <121398999+hpktz@users.noreply.github.com>
2025-03-09 20:03:47 +0000

    Update of README.md and addition of comments in main.py

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# sims4_mods_sorter
-This python program automatically arranges the .package files you download for additional items on the game.
+# 🎮 Sims4 Mods Sorter
+
+Ce programme Python organise automatiquement les fichiers `.package` que vous téléchargez pour ajouter des éléments supplémentaires dans votre jeu Sims 4.
+
+## 🎈 Pourquoi l'utiliser ?
+
+Si vous êtes un joueur de Sims 4, vous avez probablement téléchargé des mods pour ajouter de nouveaux objets à votre jeu. Cependant, il n'est pas toujours facile de trouver le bon emplacement pour ces fichiers. Ce programme vous aide à organiser vos mods de manière plus efficace !
+
+## 🛠️ Prérequis
+
+- Python 3.6 ou supérieur → [Télécharger Python](https://www.python.org/downloads/)
+- Pip → [Télécharger Pip](https://pip.pypa.io/en/stable/installing/)
+- Sims 4 installé sur votre ordinateur
+- Windows 10 ou supérieur
+
+## 📦 Installation
+
+1. Clonez le dépôt
+   ```bash
+   git clone https://github.com/hpktz/sims4_mods_sorter
+   ```
+
+2. Installez les packages requis
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+## 🎮 Comment l'utiliser
+
+1. Accédez au dossier de vos mods
+   Habituellement, les mods se trouvent ici :
+   ```bash
+   cd Documents/Electronic Arts/The Sims 4/Mods/CAS
+   ```
+
+2. Créez des catégories pour vos mods en créant des dossiers avec les noms des catégories (dans votre langue) que vous souhaitez utiliser.
+   **Exemple :**
+   ```bash
+   mkdir Accessoires # Cela créera un dossier appelé Accessoires
+   ```
+
+3. Mettez à jour les catégories en anglais dans le fichier `main.py` en modifiant la variable `categories` à la **ligne 15** avec cette syntaxe :
+   ```python
+   categories = ['<categorie1>', '<categorie2>', '<categorie3>', ...]
+   ```
+
+4. Ajoutez ensuite la traduction des catégories dans votre langue dans la variable `categories_dir` à la **ligne 17** avec cette syntaxe :
+   ```python
+   categories_dir = {
+       '<categorie1>': '<categorie1_in_your_langage>',
+       '<categorie2>': '<categorie2_in_your_langage>',
+       '<categorie3>': '<categorie3_in_your_langage>',
+       ...
+   }
+   ```
+
+5. (Optionnel) Si vous souhaitez vous assurer que le programme ne déplace pas les fichiers dans le mauvais dossier, vous pouvez ajouter des mots-clés associés aux catégories dans la variable `pre_filters` à la **ligne 30** avec cette syntaxe :
+   ```python
+   pre_filters = {
+       '<categorie1>': [
+           '<keyword1>',
+           '<keyword2>',
+           '<keyword3>',
+           ...
+       ],
+       '<categorie2>': [
+           '<keyword1>',
+           '<keyword2>',
+           '<keyword3>',
+           ...
+       ],
+       '<categorie3>': [
+           '<keyword1>',
+           '<keyword2>',
+           '<keyword3>',
+           ...
+       ],
+       ...
+   }
+   ```
+
+6. Téléchargez vos mods dans le dossier `Downloads`.
+
+7. Exécutez le programme
+   ```bash
+   python main.py
+   ```
--- main.py
+++ main.py
@@ -5,19 +5,47 @@ import re
 import logging
 import shutil
 
+# Disable all logging messages
 logging.disable(logging.CRITICAL)
 
-def mose_to_correct_dest(file_name):
+# Function to move file to the correct destination based on classification
+def move_to_correct_dest(file_name):
+    """
+    Moves a file to the correct destination directory based on its name.
+    This function uses a combination of pre-defined keyword filters and a zero-shot classification model to determine the category of the file. The file is then moved to a corresponding directory based on the identified category.
+    
+    Args:
+        file_name (str): The full path of the file to be moved.
+    
+    Returns:
+        str: A message indicating whether the file was moved or if it already exists in the destination directory.
+    
+    The function performs the following steps:
+    1. Initializes a zero-shot classification pipeline using the "valhalla/distilbart-mnli-12-3" model.
+    2. Defines a list of categories for classification.
+    3. Maps each category to its respective directory.
+    4. Defines pre-filters to quickly identify categories based on keywords in the file name.
+    5. Extracts the base file name without path and extension, and processes it to be more readable.
+    6. Checks if the file name contains any pre-filter keywords to determine the category.
+    7. If no pre-filter match is found, uses the classifier to determine the category.
+    8. Maps the identified category to the corresponding directory.
+    9. Defines the output directory path based on the identified category.
+    10. Moves the file to the output directory if it doesn't already exist, otherwise returns a message indicating the file already exists.
+    """
+    
+    # Initialize the zero-shot classification pipeline
     classifier = pipeline("zero-shot-classification", 
-                      model="valhalla/distilbart-mnli-12-3", 
-                      device=1)
+                          model="valhalla/distilbart-mnli-12-3", 
+                          device=1)
 
-    categories = ["bottom", "accessory", "jewerly", "shoes", "hair", "full body", "top", "makeup", "facial features"]
+    # Define the categories for classification
+    categories = ["bottom", "accessory", "jewelry", "shoes", "hair", "full body", "top", "makeup", "facial features"]
 
+    # Map categories to their respective directories
     categories_dir = {
         "bottom": "Bas",
         "accessory": "Accessoires",
-        "jewerly": "Bijoux",
+        "jewelry": "Bijoux",
         "shoes": "Chaussures",
         "hair": "Cheveux",
         "full body": "Ensembles",
@@ -27,9 +55,10 @@ def mose_to_correct_dest(file_name):
         "other": "Autres"
     }
 
+    # Pre-filters to quickly identify categories based on keywords
     pre_filters = {
         "bottom": ["skirt", "jeans", "pants", "leggings", "shorts", "panties"],
-        "jewerly": ["earrings", "necklace", "rings", "bracelet", "choker", "watch", "hoops"],
+        "jewelry": ["earrings", "necklace", "rings", "bracelet", "choker", "watch", "hoops"],
         "shoes": ["boots", "pumps", "strap", "sneakers"],
         "full body": ["dress", "outfit", "body", "body suit"],
         "top": ["blazer", "jacket", "vest", "tank"],
@@ -37,12 +66,14 @@ def mose_to_correct_dest(file_name):
         "skin": ["eyebrows", "freckles", "cheekmole"]
     }
 
+    # Extract the base file name without path and extension
     file_name_b = file_name.split("\\")[-1]
     file_name_b = file_name_b.rstrip('.package')
     file_name_b = file_name_b.replace('_', ' ')
     file_name_b = re.sub(r'([a-z])([A-Z])', r'\1 \2', file_name_b)
     file_name_b = file_name_b.lower()
 
+    # Check if the file name contains any pre-filter keywords
     for key_word, words in pre_filters.items():
         for word in words:
             if word in file_name_b:
@@ -53,8 +84,8 @@ def mose_to_correct_dest(file_name):
         break
     else:
         directory = None
-    
 
+    # If no pre-filter match, use the classifier to determine the category
     if not directory:
         directory = classifier(file_name, categories)
         if directory["scores"][0] > 0.25:
@@ -62,21 +93,24 @@ def mose_to_correct_dest(file_name):
         else:
             directory = "other"
     
+    # Map the category to the corresponding directory
     directory = categories_dir[directory]
 
-    output_dir = './Documents/Electronic Arts/Les Sims 4/Mods/CAS/' + directory +'/' + file_name.split("\\")[-1]
+    # Define the output directory path
+    output_dir = './Documents/Electronic Arts/Les Sims 4/Mods/CAS/' + directory + '/' + file_name.split("\\")[-1]
 
+    # Move the file to the output directory if it doesn't already exist
     if os.path.exists(output_dir):
-        return(f"Le fichier {output_dir} existe déjà.")
+        return(f"The file {output_dir} already exists.")
     else:
         shutil.move(file_name, output_dir)
-        return(f"Le fichier a été déplacé vers : {directory}")
+        return(f"The file has been moved to: {directory}")
 
-
-
-
+# Define the input directory containing the files to be processed
 input_dir = './Downloads'
+# Get a list of all .package files in the input directory
 files_pack = glob.glob(os.path.join(input_dir, '*.package'))
 
-for index, file  in enumerate(files_pack):
-    print(str(index) + "/" + str(len(files_pack)) + ":" + mose_to_correct_dest(file))
+# Process each file and move it to the correct destination
+for index, file in enumerate(files_pack):
+    print(str(index) + "/" + str(len(files_pack)) + ": " + move_to_correct_dest(file))
